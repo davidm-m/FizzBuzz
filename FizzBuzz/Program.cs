77b5257e11fbde2c8df0c1c7d436839f67555257
DavidM-M <david.may-miller@softwire.com>
2018-11-06 16:46:37 +0000

    Updated gitignore to ignore debug folders. Implemented Fizz, Buzz, Bang, and Bong rules.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,36 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (var i = 1; i <= 1000; i++)
+            {
+                var response = "";
+                if (i % 3 == 0)
+                {
+                    response += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    response += "Buzz";
+                }
+
+                if (i % 7 == 0)
+                {
+                    response += "Bang";
+                }
+
+                if (i % 11 == 0)
+                {
+                    Console.WriteLine("Bong");
+                    continue;
+                }
+
+                if (response == "")
+                {
+                    response = i.ToString();
+                }
+                Console.WriteLine(response);
+            }
         }
     }
 }
